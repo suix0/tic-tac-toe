fc23f0e51fcf5df417b44ced1ae12f4817b0e7df
suix0 <112095627+suix0@users.noreply.github.com>
2024-05-02 23:53:37 +0800

    implemented win conditions for player 1

--- script.js
+++ script.js
@@ -38,32 +38,69 @@ function gameController() {
   let gameFinish = false;
   let nextPlayer = false;
 
-  // Track the values of each rows, columns, and diagonals 
-  let boardStatus = [
-    {"01": "", "02": "", "03": ""},
-    {"11": "", "12": "", "13": ""},
-    {"21": "", "22": "", "23": ""}
-  ];
 
   while (gameFinish === false) {
   //   Prompt input and make sure it falls within the board indices
     do {
       playerInputRow = parseInt(prompt("Enter row: "));
       playerInputColumn = parseInt(prompt("Enter column: "));
-    } while (playerInputRow > 3 && playerInputRow < 1 && playerInputColumn > 3 && playerInputColumn < 1);
 
+      if (gameBoard.getBoard()[playerInputRow - 1][playerInputColumn - 1] !== " ") {
+        alert("Someone made a move there already. Try again.");
+      }
+    } while ((playerInputRow > 3 || playerInputRow < 1) && (playerInputColumn > 3 || playerInputColumn < 1));
 
     if (nextPlayer === false) {
-      gameBoard.insertPlayerInput(playerInputRow, playerInputColumn, playerOne.value);
+      gameBoard.insertPlayerInput(playerInputRow, playerInputColumn, playerOne.value);  
       nextPlayer = true;
 
     } else {
       gameBoard.insertPlayerInput(playerInputRow, playerInputColumn, playerTwo.value);
       nextPlayer = false;
     }
-  }
-  
 
+    console.table(gameBoard.getBoard());
+
+    // Check for rows if all are match for playerOne
+    // If it is, end game
+    for (let i = 0; i < 3; i++) {
+      // Implement first win conditions for playerOne
+      // Check for each row
+      if (gameBoard.getBoard()[0][i] === "X" && gameBoard.getBoard()[0][i + 1] === "X" && gameBoard.getBoard()[0][i + 2] === "X") {
+        alert("Player 1 win");
+        gameFinish = true;
+
+      } else if (gameBoard.getBoard()[1][i] === "X" && gameBoard.getBoard()[1][i + 1] === "X" && gameBoard.getBoard()[1][i + 2] === "X") {
+        gameFinish = true;
+        alert("Player 1 win");
+
+      } else if (gameBoard.getBoard()[2][i] === "X" && gameBoard.getBoard()[2][i + 1] === "X" && gameBoard.getBoard()[2][i + 2] === "X") {
+        gameFinish = true;
+        alert("Player 1 win");
+
+        // Check for each columns 
+      } else if (gameBoard.getBoard()[i][0] === "X" && gameBoard.getBoard()[i + 1][0] === "X" && gameBoard.getBoard()[i + 2][0] === "X") {
+        gameFinish = true;
+        alert("Player 1 win");
+
+      } else if (gameBoard.getBoard()[i][1] === "X" && gameBoard.getBoard()[i + 1][1] === "X" && gameBoard.getBoard()[i + 2][1] === "X") {
+        gameFinish = true;
+        alert("Player 1 win");
+
+      } else if (gameBoard.getBoard()[i][2] === "X" && gameBoard.getBoard()[i + 1][2] === "X" && gameBoard.getBoard()[i + 2][2] === "X") {
+        gameFinish = true;
+        alert("Player 1 win");
+
+        // Check for each diagonals
+      } else if (gameBoard.getBoard()[0][i] === "X" && gameBoard.getBoard()[1][i + 1] === "X" && gameBoard.getBoard()[2][i + 2] === "X") {
+        gameFinish = true;
+        alert("Player 1 win");
+      } else if (gameBoard.getBoard()[0][i + 2] === "X" && gameBoard.getBoard()[1][i + 1] === "X" && gameBoard.getBoard()[2][i] === "X") {
+        gameFinish = true;
+        alert("Player 1 win");
+      } 
+    }
+  }
 }
 
 gameController();
